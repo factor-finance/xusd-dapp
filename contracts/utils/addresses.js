8f73917d7ceac9c78f98c0974bc396ea369d294f
xusd <xusd@xusd.fi>
2021-12-21 00:30:32 -0500

    address fixups for node to work

--- contracts/utils/addresses.js
+++ contracts/utils/addresses.js
@@ -15,7 +15,7 @@ addresses.mainnet.Binance = "0x9f8c163cBA728e99993ABe7495F06c0A3c8Ac8b9";
  * But all together do. In case new ones are added update them from here:
  * https://snowtrace.io/accounts/label/binance?subcatid=3-0&size=100&start=0&col=2&order=desc
  */
-addresses.mainnet.BinanceAll = addresses.mainnet.Binance;
+addresses.mainnet.BinanceAll = "0x9f8c163cBA728e99993ABe7495F06c0A3c8Ac8b9,0x0455ea966197a69eccf5fc354b6a7896e0fe38f0,0x2d6b7235db3659c1751f342f6c80a49727bb1a1d";
 
 // Native stablecoins
 addresses.mainnet.DAIe = "0xd586e7f844cea2f87f50152665bcbc2c279d8d70";
@@ -25,9 +25,13 @@ addresses.mainnet.USDTe = "0xc7198437980c041c805a1edcba50c1ce5db95118";
 // Native stablecoins
 addresses.mainnet.TUSD = "0x1c20e891bab6b1727d14da358fae2984ed9b59eb";
 addresses.mainnet.DAI = addresses.mainnet.DAIe;
-// these are very thin
+// these are very thin, so...
 addresses.mainnet.USDC = "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e";
 addresses.mainnet.USDT = "0x9702230a8ea53601f5cd2dc00fdbc13d4df4a8c7";
+// REDEFINE
+addresses.mainnet.USDC = addresses.mainnet.USDCe
+addresses.mainnet.USDT = addresses.mainnet.USDTe
+
 
 // AAVE
 addresses.mainnet.AAVE_ADDRESS_PROVIDER =
