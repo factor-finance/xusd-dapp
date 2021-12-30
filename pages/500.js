67ca26d10acbb6e4e0b17d49b73cb4053df41d56
0xAnnapurna <xusd2+annapurna@protonmail.com>
2021-12-30 13:56:41 -0600

    output static error pages

---
+++ pages/500.js
@@ -0,0 +1,6 @@
+// FIXME: report error to sentry.
+// See https://nextjs.org/docs/advanced-features/custom-error-page#more-advanced-error-page-customizing
+
+export default function Custom500() {
+  return <h1>500 - Server-side error occurred</h1>
+}
