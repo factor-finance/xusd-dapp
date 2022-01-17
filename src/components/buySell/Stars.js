6e1a2b03d9a7edb09e66336e75f692cf5a193cd2
0xKirkjufell <96358263+0xKirkjufell@users.noreply.github.com>
2022-01-17 13:21:22 -0500

    animate star background

---
+++ src/components/buySell/Stars.js
@@ -0,0 +1,14 @@
+import classnames from 'classnames'
+
+// https://codepen.io/interaminense/pen/QyGbXL
+const StarryBackground = () => {
+  return (
+    <>
+      <div class="night"></div>
+      <div class="stars"></div>
+      <div class="twinkling"></div>
+    </>
+  )
+}
+
+export default StarryBackground
