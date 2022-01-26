2e2b485d7b31673570a14142c004cc088d4a441c
0xKirkjufell <96358263+0xKirkjufell@users.noreply.github.com>
2022-01-25 22:05:28 -0500

    replace highres with normal event

--- src/components/TransactionListener.js
+++ src/components/TransactionListener.js
@@ -138,7 +138,7 @@ const TransactionListener = ({ rpcProvider }) => {
       handlePossibleReplacedTransaction(log.transactionHash)
     })
 
-    wsProvider.on(xusd.filters.TotalSupplyUpdatedHighres(), (log, event) => {
+    wsProvider.on(xusd.filters.TotalSupplyUpdated(), (log, event) => {
       handlePossibleReplacedTransaction(log.transactionHash)
     })
 
