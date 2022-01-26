01fff87093615524e96ab2e1c3c09787e2a23dbc
0xAnnapurna <96362165+0xAnnapurna@users.noreply.github.com>
2022-01-26 17:52:40 -0600

    fix 0 display in header if balance 0

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -27,9 +27,9 @@ const DappLinks = ({ dapp, page }) => {
   const xusdBalance = useStoreState(AccountStore, (s) => s.balances['xusd'])
   const lifetimeYield = useStoreState(AccountStore, (s) => s.lifetimeYield)
   const showHistory =
+    process.env.HIDE_INACTIVE_PAGES != 'true' &&
     ((xusdBalance && parseFloat(xusdBalance) > 0) ||
-      (lifetimeYield && parseFloat(lifetimeYield) > 0)) &&
-    process.env.HIDE_INACTIVE_PAGES != 'true'
+      (lifetimeYield && parseFloat(lifetimeYield) > 0))
 
   return (
     <>
