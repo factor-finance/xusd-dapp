1c68ee84baadc57553f70482a280ab7d5dbe5e3d
0xAnnapurna <96362165+0xAnnapurna@users.noreply.github.com>
2022-02-05 00:15:30 -0600

    fix currency name/ordering in wallet sidebar (#52)

--- src/constants/Contract.js
+++ src/constants/Contract.js
@@ -1,9 +1,9 @@
 export const currencies = {
-  usdt: {
-    localStorageSettingKey: 'usdt-manual-setting',
-  },
   dai: {
     localStorageSettingKey: 'dai-manual-setting',
+  },
+  usdt: {
+    localStorageSettingKey: 'usdt-manual-setting',
   },
   usdc: {
     localStorageSettingKey: 'usdc-manual-setting',
