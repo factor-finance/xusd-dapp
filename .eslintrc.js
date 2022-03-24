1c68e180f2e2178ebc18697a8d001e6b50a90c88
0xKirkjufell <96358263+0xKirkjufell@users.noreply.github.com>
2022-03-24 17:40:21 -0400

    update linter to eslint react plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,16 +6,29 @@ module.exports = {
     amd: true,
     mocha: true,
   },
+  rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/react-in-jsx-scope': 'error',
+  },
+  plugins: ['react'],
   extends: [
-    "eslint:recommended",
-    "plugin:react/jsx-runtime"
+    'eslint:recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react/recommended',
   ],
   globals: {
-    process: "readable",
-    module: "writable"
+    process: 'readable',
+    module: 'writable',
   },
   parserOptions: {
     ecmaVersion: 11,
-    sourceType: "module",
-  }
-};
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+}
